50a56962e596d684b1d8ff686adb015da2f3078f
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-25 17:10:13 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -21,6 +21,7 @@ categories = ['00','01','02','03','04','05','06','07','08','09','10']
 
 
 def predict(model, categories, image):
+    print(image)
     img = tf.cast(image,tf.float32)
     img/=255.0
     img = tf.image.resize(img,(128,128))
