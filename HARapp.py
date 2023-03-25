ce075bc950b05606190a7f820701d83a77f4b6c1
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-25 16:36:38 +0530

    Add files via upload

---
+++ HARapp.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import PIL
+from PIL import Image
+import io
+import tensorflow as tf
+
+def load_image():
+    upload_file = st.file_uploader(label ='pick a test image')
+    if upload_file is not None:
+        image_data = upload_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+def load_model():
+    model = tf.keras.models.load_model('C:/Users/avina/Downloads/model2_ 716_val_accuracy0.84177_val_loss1.06168.h5')
+
+    return model
+
+categories = ['00','01','02','03','04','05','06','07','08','09','10']
+
+
+def predict(model, categories, image):
+    img = tf.cast(image,tf.float32)
+    img/=255.0
+    img = tf.image.resize(img,(128,128))
+
+
+
+    prob = model.predict(img)
+    
+
+    
+    for i in range(prob.shape(0)):
+        st.write(categories[prob[i]])
+
+
+
+
+
+
+def main():
+    st.title('Custom model demo')
+    model = load_model()
+    image = load_image()
+    result = st.button('Run on image')
+    if result:
+        st.write('Calculating results...')
+        predict(model, categories, image)
+
+
+if __name__ =='__main__':
+    main()
