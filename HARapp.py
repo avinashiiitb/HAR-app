03084cdb40c28cab8b267a6d551bec5c57e88335
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-26 16:13:09 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -25,7 +25,29 @@ def load_model():
     return model
 
 categories = ['00','01','02','03','04','05','06','07','08','09','10']
+import base64
 
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 def predict(model, categories, image):
     image = np.array(image)
@@ -64,7 +86,7 @@ def predict(model, categories, image):
     fig = px.pie(d, values='Probability of each class', names='Classes')
     fig.update_traces(textposition='inside', textinfo='percent+label')
     st.plotly_chart(fig)
-
+    
 
 
 
