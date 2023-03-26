40531d5ae116a9fbc101cece941282f3e5675952
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-26 16:38:08 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -49,7 +49,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('background.png')
+set_png_as_page_bg('background-1.png')
 
 def predict(model, categories, image):
     image = np.array(image)
