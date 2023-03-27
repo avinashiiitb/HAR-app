10c3d838d9f617492f7d844d1666b45784a4dc11
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-28 02:00:02 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -37,7 +37,7 @@ def load_image():
           image_data = upload_file.getvalue()
           image1 = Image.open(io.BytesIO(image_data)) 
           
-          with open(image_data, 'rb') as f:
+          with open(upload_file, 'rb') as f:
                img_to_zip = f.read()
                img_open = Image.open(io.BytesIO(img_to_zip))
                st.write(img_open)
