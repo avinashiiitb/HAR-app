b54c37d6b1cbca9de10a77f45e508577485c8aba
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-27 22:58:53 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -10,8 +10,7 @@ import pandas as pd
 import plotly.express as px
 
 def save_uploadedfile(uploadedfile):
-     #os.mkdir(".tempDir/")
-     with open(os.path.join(".tempDir/",uploadedfile.name)) as f:
+     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
          f.write(uploadedfile.getbuffer())
      return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
      
