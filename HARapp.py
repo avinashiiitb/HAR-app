7317a7ba0888be121ce0b6cfac03e326302d3fae
avinashiiitb <109304213+avinashiiitb@users.noreply.github.com>
2023-03-28 19:29:04 +0530

    Update HARapp.py

--- HARapp.py
+++ HARapp.py
@@ -18,7 +18,7 @@ import json
 def save_uploadedfile(uploadedfile):
      with open(os.path.abspath(os.path.join(".tempDir/",uploadedfile.name)),"wb") as f:
          f.write((uploadedfile).getbuffer())
-     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+     
 
 
 
